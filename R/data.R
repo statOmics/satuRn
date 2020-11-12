148953486f17a9db21ca6d18f21e094fc7e08a52
jgilis <jeroen.gilis@ugent.be>
2020-11-12 16:26:23 +0100

    include vignette

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' A DataFrame with counts derived from Tasic case study (explain how).
+#'
+#' @usage data(Tasic_counts_vignette)
+"Tasic_counts_vignette"
+
+#' A DataFrame with metadata derived from Tasic case study (explain how).
+#'
+#' @usage data(Tasic_metadata_vignette)
+"Tasic_metadata_vignette"
